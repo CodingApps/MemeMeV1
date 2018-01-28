1396594204754377f09ac1b03edfba4f153a32ed
CodingApps <codingkit@gmail.com>
2018-01-27 16:12:54 -0800

    Initial Commit

---
+++ LoadPicV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadPicV2
+//
+//  Created by Rick Mc on 1/27/18.
+//  Copyright © 2018 Rick Mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
